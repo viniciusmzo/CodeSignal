2e53e87288ce5be0c963ea938af37590eba4ad67
ViniciusMZO <vinicius_mzo@hotmail.com>
2023-08-08 20:58:20 -0300

    Intro - Exercise 02

---
+++ CodeSignal_Intro_Ex_02/Program.cs
@@ -0,0 +1,38 @@
+﻿
+int year = int.Parse(Console.ReadLine());
+Console.WriteLine(SolutionYear.ReturnCentury(year));
+
+public static class SolutionYear
+{
+    public static int ReturnCentury(int year)
+    {
+        int century;
+
+        string ageStr = year.ToString();
+        if (ageStr.Length < 4)
+        {
+            var cont = 4 - ageStr.Length;
+            var zeros = "";
+
+            for (int i = 0; i < cont; i++)
+            {
+                zeros += "0";
+            }
+            ageStr = zeros + ageStr;
+        }
+
+        string firstNums = ageStr.Substring(0, 2);
+        string lastNums = ageStr.Substring(2, 2);
+
+        if (lastNums == "00")
+        {
+            century = int.Parse(firstNums);
+        }
+        else
+        {
+            century = int.Parse(firstNums) + 1;
+        }
+
+        return century;
+    }
+}
