99dbc994ba297529614a0cee5978ae315e523cf7
ViniciusMZO <vinicius_mzo@hotmail.com>
2023-08-08 20:45:09 -0300

    Intro - Exercise 01

---
+++ CodeSignal_Intro_Ex_01/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+var param1 = int.Parse(Console.ReadLine());
+var param2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Solution.Sum(param1, param2).ToString());
+
+
+public static class Solution 
+{
+	public static int Sum(int param1, int param2) 
+	{
+        return param1 + param2;
+    }
+}
