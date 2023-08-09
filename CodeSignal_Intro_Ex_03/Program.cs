dc49f939e9827fde4db4ad22b715c084f10c6321
ViniciusMZO <vinicius_mzo@hotmail.com>
2023-08-08 22:19:26 -0300

    Intro - Exercise 03

---
+++ CodeSignal_Intro_Ex_03/Program.cs
@@ -0,0 +1,10 @@
+﻿var inputStr = Console.ReadLine();
+Console.WriteLine(Palindrome.ReturnIsPalindrome(inputStr).ToString());
+
+public static class Palindrome 
+{
+    public static bool ReturnIsPalindrome(string inputString) 
+    {
+        return inputString.SequenceEqual(inputString.Reverse());
+    }
+}
